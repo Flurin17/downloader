92f636a00edab135c99e13197810f4671cd88b3f
Flurin17 <Flurin.laim@gmail.com>
2020-02-05 15:32:37 +0100

    deleted season command

--- testdiscordbot.py
+++ testdiscordbot.py
@@ -60,81 +60,6 @@ async def movie(ctx, *args):
         embed = wrongchannelembed(args)
         await ctx.send(embed=embed)
 
-@client.command(pass_context = True)
-async def season(ctx, *args):
-    args = ' '.join(args)  
-    if ctx.message.channel.id == discordChannelId:
-        imdbIDs, seriestitles, seriesposters, downloaded, years  = imdbSeriesSearch(str(args))
-        embed = filmembed(seriestitles,downloaded, imdbIDs, years, ctx) 
-        messages.append(await ctx.send(embed=embed))
-        
-        try:
-            option = await client.wait_for('message', timeout=45, check=check(ctx.author))
-        except asyncio.TimeoutError:
-            await ctx.send("Time is up. Please start over")
-            return
-
-        if "!season" in option.content:
-            return
-
-        try:
-            optionchoosen = int(option.content)
-        except:
-            await ctx.send("Please provide a valid Option")
-        if optionchoosen <= len(downloaded) and optionchoosen >= 0:
-            print("all good")
-        else:
-            await ctx.send("Number is not in Range. Start over")
-            return
-
-        seasons, jsonseries = imdbSeriesSearchSeason(imdbIDs[optionchoosen])
-
-        embed = seasonsEmbed(seasons, seriestitles[optionchoosen], seriesposters[optionchoosen], ctx)
-        await ctx.send(embed=embed)
-
-        try:
-            option = await client.wait_for('message', timeout=45, check=check(ctx.author))
-        except asyncio.TimeoutError:
-            await ctx.send("Time is up. Please start over")
-            return
-
-        if "!season" in option.content:
-            return
-
-        try:
-            optionchoosenSeries = int(option.content)
-        except:
-            await ctx.send("Please provide a valid Option")
-        if optionchoosenSeries <= len(seasons) and optionchoosenSeries >= 1:
-            optionchoosenSeries = optionchoosenSeries -1
-            
-            await ctx.send(embed=embed)
-        else:
-            await ctx.send("Number is not in Range. Start over")
-            return
-        
-        downloadlink, downloadname, downloadsize, downloadcategory, downloadpage, seeders, leechers = getSeries(imdbIDs[optionchoosen], seasons[optionchoosenSeries]) 
-        if downloadlink == "404 No  have been found":
-            embed = discord.Embed(
-                description= "404 - No Torrent could be found!",
-                color=discord.Color.red()
-            )
-            embed.set_author(name="RARBG-Torrent")
-            embed.add_field(name="IMDB-Title", value="Your Season '{0}' isn't on RARBG!".format(seriestitles[optionchoosen]))
-            embed.set_footer(text=("Requested by {0}").format(ctx.message.author))
-            await ctx.send(embed=embed)
-
-        embed = torrentembed(downloadname, downloadpage, downloadsize, seeders, leechers, seriesposters, optionchoosen, ctx)
-        message = await ctx.send(embed=embed)
-        
-        startDownload(downloadlink, downloadcategory) #Just for testing put it over embed again
-        await asyncio.sleep(10)
-        client.loop.create_task(update(message.id, downloadlink, ctx))
-
-    else:
-        print("Wrong channel")
-        embed = wrongchannelembed(args)
-        await ctx.send(embed=embed)
 
 @client.command(pass_context = True)
 async def show(ctx, *args):
@@ -146,11 +71,9 @@ async def show(ctx, *args):
 
         embed = filmembed(seriestitles,downloaded, imdbIDs, years, ctx) 
         messages.append(await ctx.send(embed=embed))
-        
-
-
+    
         try:
-            option = await client.wait_for('message', timeout=10, check=check(ctx.author))
+            option = await client.wait_for('message', timeout=45, check=check(ctx.author))
             messages.append(option)
         except asyncio.TimeoutError:
             messages.append(await ctx.send("Time is up. Please start over"))
